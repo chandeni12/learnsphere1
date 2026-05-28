fbcbf51dea09fce7c6a7089269041122c78f8673
Chandini <chandini@gmail.com>
2026-05-28 10:13:29 +0530

    chore: add vercel and render deployment configs

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 // let MongoURL = "mongodb://127.0.0.1:27017/eLearning";
 
 let MongoURL = process.env.DB_URL;
